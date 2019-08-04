ea477c41da037843d8cc14689f44b941b5b6cfaa
Oliver Newland <onewland@gmail.com>
2019-08-03 22:05:12 -0700

    Change colors randomly

--- src/ThreeGram.js
+++ src/ThreeGram.js
@@ -15,14 +15,45 @@ export default class ThreeGram extends React.Component {
             ["although",	"the",	"nature"],
         ];
         this.cycleLength = this.ngramsList.length;
-        this.state = { counter: 0, ngram: ['0','1','2'] };
+        this.colors = ['lightgoldenrodyellow', 'lightsalmon','lightcoral','#CCFFC9','#C9CDFF','#FFC9FD'];
+        this.state = { counter: 0, ngram: ['0','1','2'], cell_colors: ['lightgoldenrodyellow', 'lightsalmon','lightcoral'] };
     }
 
     componentDidMount() {
         this.timerID = setInterval(
           () => this.tick(),
-          10000
+          5000
         );
+
+        setInterval(
+            () => this.randomColorChange(),
+            1000
+        );
+        this.tick();
+    }
+
+    randomColorChange() {
+        const randomCellIdx = Math.floor(Math.random() * 3);
+        const colorCount = this.colors.length;
+        var cell_colors = this.state.cell_colors;
+        var randomColor;
+        if(randomCellIdx === 0 || randomCellIdx === 2) {
+            const adjacent = cell_colors[1];
+            do {
+                const randomColorIdx = Math.floor(Math.random() * colorCount);
+                randomColor = this.colors[randomColorIdx];
+                cell_colors[randomCellIdx] = randomColor;
+            } while(adjacent === randomColor);
+        } else {
+            const adjacent = cell_colors[0];
+            const adjacent2 = cell_colors[2];
+            do {
+                const randomColorIdx = Math.floor(Math.random() * colorCount);
+                randomColor = this.colors[randomColorIdx];
+                cell_colors[randomCellIdx] = randomColor;
+            } while(adjacent === randomColor || adjacent2 === randomColor);
+        }
+        this.setState({ cell_colors: cell_colors })
     }
 
     tick() {
@@ -38,15 +69,16 @@ export default class ThreeGram extends React.Component {
 
     render() {
       var ngram = this.state.ngram;
+      var colors = this.state.cell_colors;
       return (
         <div className="row">
-            <div className="col-sm ngram-col no-float" id="col-left">
+            <div style={{backgroundColor: colors[0]}} className="col-sm ngram-col no-float" id="col-left">
                 <p>{ngram[0]}</p>
             </div>
-            <div className="col-sm ngram-col no-float" id="col-center">
+            <div style={{backgroundColor: colors[1]}} className="col-sm ngram-col no-float" id="col-center">
                 <p>{ngram[1]}</p>
             </div>
-            <div className="col-sm ngram-col no-float" id="col-right">
+            <div style={{backgroundColor: colors[2]}} className="col-sm ngram-col no-float" id="col-right">
                 <p>{ngram[2]}</p>
             </div>
         </div>
